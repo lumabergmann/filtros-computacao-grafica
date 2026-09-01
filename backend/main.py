edc424b4d351e6634becc480cc435de0cf90f779
Luma Bergmann <lumabergmann@gmail.com>
2026-09-01 16:14:05 -0300

    Implementando função do método post

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi.responses import Response
+from pathlib import Path
+from PIL import Image
+import io
+from filtros import processar_filtro
 
 app = FastAPI()
 
 @app.post("/processar")
-async def processar_imagem(file: UploadFile = File(...), filtro: str = Form(...)):  # O envio da imagem é obrigatório (os parâmetros recebidos não podem ser nulos)
+async def processar_imagem(file: UploadFile = File(...), filtro: str = Form(...)):  # O envio da imagem e do nome do filtro é obrigatório (os parâmetros recebidos não podem ser nulos)
+    extensoes_permitidas = {".jpg", ".jpeg", ".png", ".webp"}
+    extensao = Path(file.filename).suffix.lower()
+
+    if extensao not in extensoes_permitidas:
+        raise HTTPException(status_code = 400, detail = f"Extensão de arquivo {extensao} não é permitida. Tipos aceitos: {', '.join(extensoes_permitidas)}.")
+
+    try:
+        imagem_original = Image.open(io.BytesIO(file))  # Salva os arquivos na memória RAM (temporária)
+
+        imagem_processada = processar_filtro(imagem_original, filtro)
+
+        # Salvando na memória RAM
+        buffer_saida = io.BytesIO()
+        imagem_processada.save(buffer_saida, format="PNG")  # Aqui deve ocorrer a escolha do formato. TODO: tornar a extensão da saída igual ao da entrada
+
+        return Response(content=buffer_saida.getvalue(), media_type="image/png")  # Retorna tudo que está armazenado no buffer
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao processar imagem: {str(e)}")
+
     
